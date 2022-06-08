170e58742e094b3b2e6ae84c3d4fddfab2219c78
Mr Dev <mrdev0803@gmail.com>
2022-06-08 06:34:47 -0700

    feat: build layout with fixed header and footer

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -3,7 +3,11 @@ import React from "react";
 
 // Dashboard Component
 const DashboardComponent: React.FC = () => {
-  return <h1>Welcome to Dashboard!!!</h1>;
+  return (
+    <div className="mt-16 mb-16">
+      <h1>Welcome to Dashboard!!!</h1>
+    </div>
+  );
 };
 
 export default DashboardComponent;
--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -3,7 +3,11 @@ import React from "react";
 
 // Header Section
 const HeaderSection: React.FC = () => {
-  return <h1>Header!!!</h1>;
+  return (
+    <div className="flex flex-col bg-indigo-700 p-4 w-full fixed top-0">
+      <h1 className="text-3xl font-bold text-white">React Testing Guide</h1>
+    </div>
+  );
 };
 
 export default HeaderSection;
